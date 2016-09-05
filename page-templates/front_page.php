31209da69a7ecb01cc05133df0c3a406fcbc3269
Bernhard Kau <bernhard.kau@vcat.de>
2016-09-05 12:14:45 +0200

    Added the possibility to add an iframe to the front page header

--- page-templates/front_page.php
+++ page-templates/front_page.php
@@ -14,29 +14,40 @@ global $post; ?>
 		<main id="main" class="site-main" role="main">
 			<?php
 			while ( have_posts() ) : the_post();
-				// we'll return a random attachment from image and videos background lists, if one is present
-				$the_random_hero = listable_get_random_hero_object();
-				$has_image       = false; ?>
+				//get an immomento iframe
+				$immomento_iframe_src = get_post_meta( get_the_ID(), 'immomento_iframe_src', true );
 
-				<?php if ( ( empty( $the_random_hero ) || property_exists( $the_random_hero, 'post_mime_type' ) || strpos( $the_random_hero->post_mime_type, 'video' ) !== false ) && is_object( $the_random_hero ) && property_exists( $the_random_hero, 'post_mime_type' ) && strpos( $the_random_hero->post_mime_type, 'image' ) !== false ) {
-					$has_image = wp_get_attachment_url( $the_random_hero->ID );
-				} ?>
+				if ( empty( $immomento_iframe_src ) ) {
+					// we'll return a random attachment from image and videos background lists, if one is present
+					$the_random_hero = listable_get_random_hero_object();
+					$has_image       = false;
+
+					if ( ( empty( $the_random_hero ) || property_exists( $the_random_hero, 'post_mime_type' ) || strpos( $the_random_hero->post_mime_type, 'video' ) !== false ) && is_object( $the_random_hero ) && property_exists( $the_random_hero, 'post_mime_type' ) && strpos( $the_random_hero->post_mime_type, 'image' ) !== false ) {
+						$has_image = wp_get_attachment_url( $the_random_hero->ID );
+					}
+				}
+				?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header has-image">
-						<div class="entry-featured"<?php if ( ! empty( $has_image ) ) {
-							echo ' style="background-image: url(' . listable_get_inline_background_image( $has_image ) . ');"';
-						} ?>>
-							<?php if ( ! empty( $the_random_hero ) && property_exists( $the_random_hero, 'post_mime_type' ) && strpos( $the_random_hero->post_mime_type, 'video' ) !== false ) {
-								$mimetype = str_replace( 'video/', '', $the_random_hero->post_mime_type );
-								if ( has_post_thumbnail( $the_random_hero->ID ) ) {
-									$image = wp_get_attachment_url( get_post_thumbnail_id( $the_random_hero->ID ) );
-									$poster = ' poster="' . $image . '" ';
-								} else {
-									$poster = ' ';
-								}
-								echo do_shortcode( '[video ' . $mimetype . '="' . wp_get_attachment_url( $the_random_hero->ID ) . '"' . $poster . 'loop="true" autoplay="true"][/video]' );
-							} ?>
-						</div>
+						<?php if ( ! empty( $immomento_iframe_src ) ) :?>
+							<iframe id="immomento_iframe" class="immomento-iframe" name="immomento-iframe" src="<?php echo esc_attr( $immomento_iframe_src ); ?>" width="100%" height="600" frameborder="0" border="0" allowtransparency="true"></iframe>
+						<?php else : ?>
+							<div class="entry-featured"<?php if ( ! empty( $has_image ) ) {
+								echo ' style="background-image: url(' . listable_get_inline_background_image( $has_image ) . ');"';
+							} ?>>
+								<?php if ( ! empty( $the_random_hero ) && property_exists( $the_random_hero, 'post_mime_type' ) && strpos( $the_random_hero->post_mime_type, 'video' ) !== false ) {
+									$mimetype = str_replace( 'video/', '', $the_random_hero->post_mime_type );
+									if ( has_post_thumbnail( $the_random_hero->ID ) ) {
+										$image = wp_get_attachment_url( get_post_thumbnail_id( $the_random_hero->ID ) );
+										$poster = ' poster="' . $image . '" ';
+									} else {
+										$poster = ' ';
+									}
+									echo do_shortcode( '[video ' . $mimetype . '="' . wp_get_attachment_url( $the_random_hero->ID ) . '"' . $poster . 'loop="true" autoplay="true"][/video]' );
+								} ?>
+							</div>
+						<?php endif; ?>
+
 						<div class="header-content">
 							<h1 class="page-title"><?php the_title(); ?></h1>
 
